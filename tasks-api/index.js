83f766fd178783cbea8cfd5f262df4d13d153662
luisgustavo <luis.gustav0@live.com>
2018-10-27 14:28:04 -0300

    primeiro commit

--- tasks-api/index.js
+++ tasks-api/index.js
@@ -1,58 +1,13 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const db = require('./config/db')
+const consign = require('consign')
 
-function meuJson() {
-    return (req, res, next) => {
-        console.log('Meu middlewere...')
-        next()
-    }
-}
-app.use(meuJson())
-app.use(bodyParser.json())
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
-app.get('/cliente', (req, res, next) => {
-    res.status(200).send(
-        '[' +
-            '{' +
-                '\"id\": 1,' +
-                '\"nome\": \"Luis Gustavo\"' +
-            '},' +
-            '{' +
-                '\"id\": 2,' +
-                '\"nome\": \"André\"' +
-            '}' +
-        ']'
-    )
-})
-
-app.get('/cliente/:id', (req, res, next) => {
-    var strJson = '';
-
-    if (req.params.id == 1) {
-        strJson =   '{' +
-                        '\"id\": 1,' +
-                        '\"nome\": \"Luis Gustavo\"' +
-                    '}';
-    } else if (req.params.id == 1) {
-        strJson =   '{' +
-                        '\"id\": 2,' +
-                        '\"nome\": \"André\"' +
-                    '}';
-    } else {
-        strJson = 'Não encontrado!'
-    }
-
-    res.status(200).send(
-       strJson
-    )
-})
-
-app.post('/cliente', (req, res) => {
-    res.status(200).send(
-       req.body
-    )
-})
+app.db = db
 
 app.listen(3000, () => {
     console.log('Backend executando...')
